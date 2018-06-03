80e0e0987d04193e3c359bff84fe2b061245f5b3
DiaKnowledge <ogasawara.no.nexus7@gmail.com>
2018-06-03 18:40:42 +0900

    rubocop対応

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -fs $(cd $(dirname $0) && pwd)/vimrc ~/.vimrc
 ln -fs $(cd $(dirname $0) && pwd)/vimrc.orig ~/.vimrc.orig
 ln -fs $(cd $(dirname $0) && pwd)/vimrc.emacs ~/.vimrc.emacs
 ln -fs $(cd $(dirname $0) && pwd)/zshrc ~/.zshrc
+ln -fs $(cd $(dirname $0) && pwd)/rubocop.yml ~/.rubocop.yml
